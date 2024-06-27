5526eb456c459987d75955e67d66c31e1511e719
campbell-frost <campbellsfrost@gmail.com>
2024-06-27 17:28:25 -0400

    Updated app to include task history

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,80 +1,66 @@
-import React, { useState } from "react";
+import { useState, ChangeEvent, FormEvent } from "react";
 import { IncrementTime, GetRandomAffirmation, GetRandomJr } from "../../wailsjs/go/main/App";
 import { Input } from "./ui/input";
 
-function App() {
-  const [time, setTime] = useState(0);
-  const [affirmation, setAffirmation] = useState("");
-  const [jr, setJr] = useState("");
-  const [inputValue, setInputValue] = useState("");
-  const [error, setError] = useState("");
+interface HistoryEntry {
+  time: number;
+  affirmation: string;
+  jr: string;
+}
+
+export default function App() {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const [history, setHistory] = useState<HistoryEntry[]>([]);
 
   const incrementTime = async (time: number) => {
     try {
       const result = await IncrementTime(time);
-      setTime(result);
-      getRandomAffirmation();
-      getRandomJr();
-    } catch (e) {
-      console.log("an error occurred", e);
+      const newAffirmation = await GetRandomAffirmation();
+      const newJr = await GetRandomJr();
+
+      setHistory(prevHistory => [
+        { time: result, affirmation: newAffirmation, jr: newJr },
+        ...prevHistory
+      ]);
+
+    } catch (e: any) {
+      setError(e.toString());
     }
     setInputValue("");
   };
 
-  const getRandomAffirmation = async () => {
-    try {
-      const result = await GetRandomAffirmation();
-      setAffirmation(result);
-    } catch (e) {
-      if (e) {
-        setError(e.toString());
-      }
-    }
-  };
-
-  const getRandomJr = async () => {
-    try {
-      const result = await GetRandomJr();
-      setJr(result);
-    } catch (e) {
-      if (e) {
-        setError(e.toString());
-      }
-    }
-  };
-
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     incrementTime(parseInt(inputValue, 10));
   };
 
   return (
-    <div className="container  min-h-screen grid mx-auto pt-3 relative">
-
+    <div className="container min-h-screen grid mx-auto py-3 relative">
       <div className="flex items-start">
-        <div className=" font-bold flex-1 flex flex-col  space-y-4">
-          <h5>Enter time worked</h5>
-          <form onSubmit={handleSubmit} className="flex  space-y-2 items-start">
-            <Input value={inputValue} onChange={handleChange} type="text" className="w-full" />
-
+        <div className="font-bold flex-1 flex flex-col space-y-4">
+          <h5 className="text-2xl flex justify-center">Enter time worked</h5>
+          <form onSubmit={handleSubmit} className="flex space-y-2 items-start">
+            <Input value={inputValue} onChange={handleChange} type="text" className="w-full rounded-lg" />
           </form>
-          <h1 className="justify-center flex text-center text-3xl">{time} minutes </h1>
-          {affirmation &&
-            <p>"{affirmation}"
-              <br />- {jr}
-            </p>
-          }
-          {error &&
-            <h5>error</h5>}
+          {history.map((entry, index) => (
+            <div key={index}>
+              <h1 className="justify-center flex text-center text-3xl pb-3">{entry.time} minutes </h1>
+              <p>
+                "{entry.affirmation}"
+                <br />- {entry.jr}
+              </p>
+            </div>
+          ))}
+          {error && <h5>error</h5>}
         </div>
       </div>
     </div>
   );
 }
 
-export default App;
+
